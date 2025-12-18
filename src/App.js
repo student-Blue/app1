746f3ff8376ee5a4fa46af7e572126ff7f636799
student-Blue <s6703052411074@email.kmutnb.ac.th>
2025-12-18 11:57:23 +0700

    Update App.js with css inline
    update App.js with css inline

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const divStyle = {
+    color: 'red',
+    backgroundColor: 'powderblue',
+    fontSize: 'larger',
+    padding: '3px'
+  };
   return (
-    <table border="1">
-      <tr>
-        <th>Product</th>
-        <th>Price</th>
-      </tr>
-      <tr>
-        <td>React</td>
-        <td>320</td>
-      </tr>
-      <tr>
-        <td>React native</td>
-        <td>340</td>
-      </tr> 
-    </table>
+  <div style={divStyle}>Hello React</div>  
   );
 }
 
