54641e0b342eaa11a1443f0668ab7b5a3ae507dc
student-Blue <s6703052411074@email.kmutnb.ac.th>
2025-12-25 11:29:41 +0700

    class components
    class components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Content, Footer, Header } from './func-components';
+import Calender from './class-components';
 
 function App() {
   return (
     <>
       <Header/>
+      <p><center><Calender/></center></p>
       <Content/>
       <Footer/>
     </>
