e0d8405c8147f638f89be22ddeb4a62786de5b43
student-Blue <s6703052411074@email.kmutnb.ac.th>
2026-01-29 11:15:34 +0700

    Create content2 & header2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { userContext } from './context'
-import Context from './context-contents';
+import Context2 from './context-contents2';
+import Header2 from './context-header2';
 export default function App() {
+  let [user, setUser] = React.useState()
   return (
-    <userContext.Provider value={'Arunburapha'}>
-      <Context />
+    <userContext.Provider value={[user, setUser]}>
+      <Header2 />
+      <Context2 />
     </userContext.Provider>
   )
 }
