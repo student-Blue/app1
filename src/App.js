bd84910bef7e3177fb8b8574223ad5fbe5740ca2
Blue <arunburapha.k@gmail.com>
2026-02-12 10:02:45 +0700

    Create navlink

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import React from 'react';
-import { userContext } from './context'
-import Context2 from './context-contents2';
-import Header2 from './context-header2';
-export default function App() {
-  let [user, setUser] = React.useState()
-  return (
-    <userContext.Provider value={[user, setUser]}>
-      <Header2 />
-      <Context2 />
-    </userContext.Provider>
-  )
+import React from 'react'
+import Rounter2 from './router2'
+ 
+export default function App(){
+  return <Rounter2/>
 }
